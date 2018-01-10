33c67f7645b4d67573292f2bd12e6f8215501dea
Piero Collevecchio <pierokoll@gmail.com>
2018-01-10 19:44:13 +0100

    Add database table to host buoys

---
+++ app/src/main/java/it/univaq/ing/myshiprace/Database/DBHelper.java
@@ -0,0 +1,68 @@
+package it.univaq.ing.myshiprace.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.List;
+
+import it.univaq.ing.myshiprace.model.Boa;
+
+/**
+ * MyService
+ * Created by leonardo on 10/11/17.
+ * <p>
+ * BiTE s.r.l.
+ * contact info@bitesrl.it
+ */
+
+public class DBHelper extends SQLiteOpenHelper
+{
+
+    private static final String NAME = "myDatabase.db";
+    private static final int VERSION = 1;
+
+    private static DBHelper instance = null;
+
+    public static DBHelper get(Context context)
+    {
+        return instance == null ? instance = new DBHelper(context) : instance;
+    }
+
+    private DBHelper(Context context)
+    {
+        super(context, NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        TableBoa.create(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        TableBoa.upgrade(db);
+    }
+
+    public void save(Boa boa)
+    {
+        TableBoa.save(getWritableDatabase(), boa);
+    }
+
+    public void update(Boa boa)
+    {
+        TableBoa.update(getWritableDatabase(), boa);
+    }
+
+    public void delete(Boa boa)
+    {
+        TableBoa.delete(getWritableDatabase(), boa);
+    }
+
+    public List<Boa> getAll()
+    {
+        return TableBoa.getAll(getReadableDatabase());
+    }
+}
---
+++ app/src/main/java/it/univaq/ing/myshiprace/Database/TableBoa.java
@@ -0,0 +1,112 @@
+package it.univaq.ing.myshiprace.Database;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.univaq.ing.myshiprace.model.Boa;
+
+/**
+ * MyService
+ * Created by leonardo on 10/11/17.
+ * <p>
+ * BiTE s.r.l.
+ * contact info@bitesrl.it
+ */
+
+public class TableBoa
+{
+
+    private static final String TABLE_NAME = "boas";
+
+    private static final String ID = "id";
+    private static final String ORDER = "order";
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
+
+    public static void create(SQLiteDatabase db)
+    {
+        String sql = "CREATE TABLE " + TABLE_NAME + "(" +
+                ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                ORDER + "INTEGER, " +
+                LATITUDE + " NUMERIC, " +
+                LONGITUDE + " NUMERIC" +
+                ")";
+        db.execSQL(sql);
+    }
+
+    public static void drop(SQLiteDatabase db)
+    {
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(sql);
+    }
+
+    public static void upgrade(SQLiteDatabase db)
+    {
+        drop(db);
+        create(db);
+    }
+
+    public static void save(SQLiteDatabase db, Boa boa)
+    {
+
+        ContentValues values = new ContentValues();
+        values.put(ORDER, boa.getOrder());
+        values.put(LATITUDE, boa.getLatitude());
+        values.put(LONGITUDE, boa.getLongitude());
+        long id = db.insert(TABLE_NAME, null, values);
+        if (id != -1) boa.setId((int) id);
+    }
+
+    public static boolean update(SQLiteDatabase db, Boa boa)
+    {
+
+        ContentValues values = new ContentValues();
+        values.put(ORDER, boa.getOrder());
+        values.put(LATITUDE, boa.getLatitude());
+        values.put(LONGITUDE, boa.getLongitude());
+//        int rows = db.update(TABLE_NAME, values, ID +"= ?", new String[]{ String.valueOf(boa.getId())} );
+        return db.update(TABLE_NAME, values, ID + "=" + boa.getId(), null) == 1;
+    }
+
+    public static boolean delete(SQLiteDatabase db, Boa boa)
+    {
+
+        return db.delete(TABLE_NAME, ID + "=" + boa.getId(), null) == 1;
+    }
+
+    public static List<Boa> getAll(SQLiteDatabase db)
+    {
+
+        List<Boa> boas = new ArrayList<>();
+
+        String sql = "SELECT * FROM " + TABLE_NAME + " ORDER BY " + ORDER + " ASC";
+        Cursor cursor = null;
+        try
+        {
+            cursor = db.rawQuery(sql, null);
+            while (cursor.moveToNext())
+            {
+                Boa boa = new Boa();
+                boa.setId((int) cursor.getLong(cursor.getColumnIndex(ID)));
+                boa.setOrder(cursor.getInt(cursor.getColumnIndex(ORDER)));
+                boa.setLatitude(cursor.getDouble(cursor.getColumnIndex(LATITUDE)));
+                boa.setLongitude(cursor.getDouble(cursor.getColumnIndex(LONGITUDE)));
+                boas.add(boa);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (cursor != null) cursor.close();
+        }
+
+        return boas;
+    }
+}
