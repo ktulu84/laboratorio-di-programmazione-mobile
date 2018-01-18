cbea07a3b8fb6b77743fb7ccaf8c7b63f4ed9bdc
Piero Collevecchio <pierokoll@gmail.com>
2018-01-18 15:51:15 +0100

    add appBar to settings activity

--- app/src/main/java/it/univaq/ing/myshiprace/SettingsActivity.java
+++ app/src/main/java/it/univaq/ing/myshiprace/SettingsActivity.java
@@ -1,13 +1,13 @@
 package it.univaq.ing.myshiprace;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by ktulu on 15/01/18.
  */
 
-public class SettingsActivity extends Activity
+public class SettingsActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
